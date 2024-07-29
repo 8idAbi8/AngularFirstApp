ae8685035f4dffe05cb339b272e6e3526972f0ac
8idAbi8 <i.l.t.l.i@hotmail.it>
2024-07-29 14:33:56 +0200

    angular-cli components interfaces input

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent,
+  ],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <!-- housingLocation property binding:
+          When adding a property binding to a component tag, we use the [attribute] = "value" syntax to notify Angular 
+          that the assigned value should be treated as a property from the component class and not a string value.
+          The value on the right handside is the name of the property from the HomeComponent.      
+
+          You added a new property binding and passed in a reference to a class property. 
+          Now, the HousingLocationComponent has access to data that it can use to customize the component's display.
+    -->
+      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+    </section>    
+    `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+  /*
+    You have to add the ! because the input is expecting the value to be passed. 
+    In this case, there is no default value. In our example application case we know that the value will be passed in - this is by design. 
+    The exclamation point is called the non-null assertion operator and it tells the TypeScript compiler 
+    that the value of this property won't be null or undefined.
+  */
+  @Input() housingLocation!: HousingLocation;
+}
