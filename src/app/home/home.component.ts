c4513045bcb973a489d2e7aa08300223566f642b
8idAbi8 <i.l.t.l.i@hotmail.it>
2024-08-02 10:53:52 +0200

    add angular service that provides static data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -24,25 +25,35 @@ import { HousingLocation } from '../housinglocation';
           that the assigned value should be treated as a property from the component class and not a string value.
           The value on the right handside is the name of the property from the HomeComponent.      
 
+          Process of sharing data from the parent component to the child component 
+          by binding data to those properties in the template using property binding.
+          Property binding enables you to connect a variable to an Input in an Angular template. 
+          The data is then dynamically bound to the Input.
+
           You added a new property binding and passed in a reference to a class property. 
-          Now, the HousingLocationComponent has access to data that it can use to customize the component's display.
+          Now, the HousingLocationComponent (child component) has access to data that it can use to customize the component's display.
+
+          Note, using the ngFor, in the code [housingLocation] = "housingLocation" the housingLocation value now refers to the variable used in the ngFor directive. 
+          Before this change (<app-housing-location [housingLocation]="housingLocation"></app-housing-location>), it referred to the property on the HomeComponent class.
     -->
-      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+    <app-housing-location
+      *ngFor="let housingLocation of housingLocationList"
+      [housingLocation]="housingLocation">
+    </app-housing-location>
     </section>    
     `,
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
-}
+/* add an array of HousingLocation. instances of the interface (HousingLocation) in the HomeComponent */
+    export class HomeComponent {
+      readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+    
+      housingLocationList: HousingLocation[] = [];
+      housingService: HousingService = inject(HousingService);
+
+      // the constructor is the first function that runs when this component is created.
+      constructor() {
+        this.housingLocationList = this.housingService.getAllHousingLocations();
+      }
+    }
